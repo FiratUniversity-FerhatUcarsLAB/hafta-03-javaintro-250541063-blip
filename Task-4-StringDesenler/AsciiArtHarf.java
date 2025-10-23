fec5db9ef4ddae2aca80b4d22b242baaa856441d
250541063-blip <250541063@firat.edu.tr>
2025-10-23 09:32:53 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("========");
+        System.out.println("=");
+        System.out.println("=");
+        System.out.println("=");
+        System.out.println("=");
+        System.out.println("=====");
+        System.out.println("=");
+        System.out.println("=");
+        System.out.println("=");
+        System.out.println("=");
+    }
+}
