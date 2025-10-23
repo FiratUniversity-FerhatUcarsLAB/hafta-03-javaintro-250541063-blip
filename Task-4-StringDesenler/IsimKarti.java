9dec62f3050223cdfd63dab58d363a065c034f67
250541063-blip <250541063@firat.edu.tr>
2025-10-23 09:13:24 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsımKartı {
+    public static void main(String[] args) {
+        String ad = "Fatma";
+        String soyad = "Türkmen";
+        String ogrno = "250541063";
+        String bolum = "Yazılım Mühendisliği";
+        
+        System.out.println("================");
+        System.out.println("ÖĞRENCİ BİLGİLERİ");
+        System.out.println("================");
+        System.out.println("Ad\t\t: " + ad);
+        System.out.println("Soyad\t\t: " + soyad);
+        System.out.println("No\t\t: " + ogrno);
+        System.out.println("Bölüm\t\t: " + bolum);
+        System.out.println("================");
+    }
+}
