70b40df08d5ca02df6372771ac7034897f3da727
250541063-blip <250541063@firat.edu.tr>
2025-10-23 08:50:12 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,11 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("JJJJJJJJJJJJJ");
+        System.out.println("JJJ\\/JJJ\\/JJJ");
+        System.out.println("JAAAAAVAAA");
+        System.out.println("JAAAAAVAAA");
+        System.out.println("JAVAVAVA");
+        System.out.println("JA   VA");
+        System.out.println("  V  ");
+    }
+}
