b2106a7e459bc9e1b380864f2d06d529ba0a1103
250541063-blip <250541063@firat.edu.tr>
2025-10-23 08:54:45 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,8 @@
+public class Ikislem {
+    public static void main(String[] args) {
+        double payda = 5.5 - 0.5;
+        double pay = 2.5 * 2.5;
+        double sonuc = payda / pay;
+        System.out.printf("5.5/n", sonuc); // Not: Görselde "5.5/n" kısmı tam net değil ama "%.4f\n" olması daha muhtemeldir. Görseldeki gibi yazdım.
+    }
+}
